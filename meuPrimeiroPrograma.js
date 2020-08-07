ca1974b46a5c4d9c65f74cd6439e2def4916cca4
Simone Cabral <simonecabral.br@gmail.com>
2020-08-07 20:37:02 -0300

    meu prmeiro programa

---
+++ meuPrimeiroPrograma.js
@@ -0,0 +1,7 @@
+console.log("Meu primeiro programa: trabalhando com variáveis");
+
+const idade = 45;
+console.log("idade: " + idade);
+console.log("idade: " + (idade + 2));
+const idadeSomada = idade + 2;
+console.log("A idade somada é: " + idadeSomada);
