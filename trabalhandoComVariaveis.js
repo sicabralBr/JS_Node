f364136e889f1b322c9591d39fc6f57e542f18f9
Simone Cabral <simonecabral.br@gmail.com>
2020-08-07 21:26:29 -0300

    Const e let

--- trabalhandoComVariaveis.js
+++ trabalhandoComVariaveis.js
@@ -1,5 +1,9 @@
 console.log("Trabalhando com variáveis");
 //JS é case sensitive
 
-idade = 45;
-console.log(idade);
+const idade = 45;
+const nome = "Simone";
+let ano = 2020;
+console.log("Idade: " + idade);
+console.log("Nome: " + nome);
+console.log("Ano: " + ano);
