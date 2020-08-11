1906a781177f9d4777df519c767d632f1fd5739c
Simone Cabral <simonecabral.br@gmail.com>
2020-08-11 19:19:08 -0300

    Exemplos novos

--- conversaoDeTipos.js
+++ conversaoDeTipos.js
@@ -3,3 +3,6 @@ console.log("Conversão de Tipos");
 console.log("2" + "2");
 console.log(parseInt("2") + parseInt("2"));
 console.log("Ano " + 2020);
+
+console.log("10" / "2");
+console.log("Ricardo" / "2"); //Not  a Number
