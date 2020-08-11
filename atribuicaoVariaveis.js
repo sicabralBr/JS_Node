06b3af8b6d853af5682b16dd4b600193ea7fbe20
Simone Cabral <simonecabral.br@gmail.com>
2020-08-11 20:01:37 -0300

    Exemplos Variaveis

--- atribuicaoVariaveis.js
+++ atribuicaoVariaveis.js
@@ -1,11 +1,19 @@
 console.log("Atribuição de Variáveis");
 const idade = 45;
-let nome = "Simone";
+const nome = "Simone";
 const sobrenome = "Cabral";
 
 console.log(nome, sobrenome);
 console.log(nome + " " + sobrenome);
 console.log(`Meu nome é ${nome} ${sobrenome}`);
 
-nome = nome + sobrenome;
-console.log(nome);
+let contador = 0;
+contador = contador + 1;
+
+const nomeCompleto = nome + sobrenome;
+console.log(nomeCompleto);
+
+let idade; //declarando variável
+idade = 45; //atribuindo valor
+idade = idade + 1;
+console.log(idade);
